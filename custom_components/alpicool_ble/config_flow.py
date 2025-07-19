c29a80a05ce3aa2553d12a8b31b34ff07b04f700
Matthias <48429291+Gruni22@users.noreply.github.com>
2025-07-19 20:31:19 +0200

    no message

--- custom_components/alpicool_ble/config_flow.py
+++ custom_components/alpicool_ble/config_flow.py
@@ -61,6 +61,10 @@ class AlpicoolConfigFlow(ConfigFlow, domain=DOMAIN):
 
 class AlpicoolOptionsFlow(OptionsFlow):
     """Handle an options flow for Alpicool BLE."""
+    def __init__(self, config_entry: ConfigEntry) -> None:
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
     async def async_step_init(self, user_input: dict[str, Any] | None = None) -> FlowResult:
         """Manage the options."""
         if user_input is not None:
